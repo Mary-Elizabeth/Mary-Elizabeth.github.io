240edc4a55786602875a90e87380b23c78bc64b1
Mary-Elizabeth <marydettenmaier@gmail.com>
2017-02-15 20:29:09 -0700

    Added additional files needed

---
+++ buttons.js
@@ -0,0 +1,19 @@
+function showAssignment4() {
+   document.getElementById('assignment4').style.display = "block";
+   document.getElementById('assignment5').style.display = "none";
+      document.getElementById('gif').style.display = "none";
+
+}
+
+function showAssignment5() {
+   document.getElementById('assignment5').style.display = "block";
+   document.getElementById('assignment4').style.display = "none";
+      document.getElementById('gif').style.display = "none";
+
+}
+function showAssignmentGif() {
+   document.getElementById('gif').style.display = "block";
+   document.getElementById('assignment4').style.display = "none";
+   document.getElementById('assignment5').style.display = "none";
+
+}
